bbf127f777a899fe2e59fa7de42d1f55b60d5736
Kyle <kylebing@163.com>
2019-03-20 09:30:27 +0800

    新加创建表的方法和目录，即安装目录

---
+++ install/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kyle
+ * Date: 2019-03-19
+ * Time: 23:26
+ */
+
+require '../portal/common.php';
+require '../portal/class/MSql.php';
+
+// Todo
--- portal/class/MSql.php
+++ portal/class/MSql.php
@@ -18,6 +18,33 @@ define('PASSWORD',  'nnqi');
  class MSql
 {
 
+    public static $CREATEDIARIES = "CREATE TABLE `diaries` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `create_date` datetime NOT NULL,
+                                  `content` varchar(255) NOT NULL,
+                                  `action` enum('add','update','delete','synced') NOT NULL DEFAULT 'add',
+                                  `category` enum('life','study','work','sport','bigevent','others') NOT NULL DEFAULT 'life',
+                                  `modify_date` datetime DEFAULT NULL,
+                                  `date` datetime NOT NULL,
+                                  PRIMARY KEY (`id`)
+                                ) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;";
+    public static $CREATEUNKNOWLOG = "CREATE TABLE `unknow_login_log` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `date` datetime NOT NULL,
+                                  `email` varchar(50) NOT NULL,
+                                  `password` varchar(30) NOT NULL,
+                                  PRIMARY KEY (`id`)
+                                ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;";
+    public static $CREATEUSERS = "CREATE TABLE `users` (
+                                  `uid` int(11) NOT NULL AUTO_INCREMENT,
+                                  `email` varchar(50) NOT NULL,
+                                  `password` varchar(100) NOT NULL,
+                                  `last_visit_time` datetime DEFAULT NULL,
+                                  `name` varchar(50) DEFAULT NULL,
+                                  `register_time` datetime DEFAULT NULL,
+                                  PRIMARY KEY (`uid`,`email`)
+                                ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;";
+
 
      /************************* 日记操作 *************************/
 
