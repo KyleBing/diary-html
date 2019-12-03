4ee49e49d8bc2767c9b120536e7c6544feb00373
KyleBing <kylebing@163.com>
2019-12-03 18:37:26 +0800

    调整结构，添加温度验证

--- html/js/diary.js
+++ html/js/diary.js
@@ -68,6 +68,8 @@ function getAuthorization() {
     }
 }
 
+
+
 // 删除cookie
 function deleteAuthorization() {
     $.removeCookie(COOKIE.email,{path: '/'});
@@ -156,4 +158,28 @@ const WEEKDAY = {
     5: '周五',
     6: '周六',
 };
+
+
+// 格式化时间，输出字符串
+function dateFormatter (date, formatString) {
+    formatString = formatString? formatString : 'yyyy-MM-dd hh:mm:ss';
+    let dateRegArray = {
+        "M+": date.getMonth() + 1,                      // 月份
+        "d+": date.getDate(),                           // 日
+        "h+": date.getHours(),                          // 小时
+        "m+": date.getMinutes(),                        // 分
+        "s+": date.getSeconds(),                        // 秒
+        "q+": Math.floor((date.getMonth() + 3) / 3),    // 季度
+        "S": date.getMilliseconds()                     // 毫秒
+    };
+    if (/(y+)/.test(formatString)) {
+        formatString = formatString.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (let section in dateRegArray) {
+        if (new RegExp("(" + section + ")").test(formatString)) {
+            formatString = formatString.replace(RegExp.$1, (RegExp.$1.length === 1) ? (dateRegArray[section]) : (("00" + dateRegArray[section]).substr(("" + dateRegArray[section]).length)));
+        }
+    }
+    return formatString;
+}
 
