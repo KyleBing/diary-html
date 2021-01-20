3f91ea5920e52bdd96a4ccc13e667ffe9f129180
KyleBing <kylebing@163.com>
2021-01-20 09:11:34 +0800

    添加年度日记总数

--- portal/class/StatisticMonth.php
+++ portal/class/StatisticMonth.php
@@ -5,11 +5,17 @@ class StatisticMonth
 {
     var $year;
     var $months;
+    var $count;
 
     public function __construct(int $year, array $months)
     {
         $this->year = $year;
         $this->months = $months;
+        $sumOfYear = 0;
+        foreach ($months as $name => $item){
+            $sumOfYear = $sumOfYear + (int)$item['count'];
+        }
+        $this->count = $sumOfYear;
     }
 }
 
