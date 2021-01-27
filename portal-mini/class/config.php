3f6332ce3522ccca5c767d82f509aea60d8da333
KyleBing <kylebing@163.com>
2021-01-27 10:37:11 +0800

    fix:add

--- portal-mini/class/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST',          'localhost');       // 数据库地址
-define('PORT',          '3306');            // 数据库端口
-define('DATABASE',      'diary');           // 数据库名
-define('USER',          '---');            // 数据库用户名
-define('PASSWORD',      '---');            // 数据库密码
-define('INVITATION',    'kylebingooOO');    // 邀请码，注册用户时使用，
